06ba1373fab4b420b1342b1a5ae8e546dc06e5cd
tsegaw wayessa <tsegaw.grace@gmail.com>
2025-12-10 11:12:26 +0300

    Advanced Data Handling with React Query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent";
 
-function App() {
-  return (
-    <div>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+// Create a React Query client
+const queryClient = new QueryClient();
+
+const App = () => {
+  return React.createElement(
+    QueryClientProvider,
+    { client: queryClient },
+    React.createElement(PostsComponent, null)
   );
-}
+};
 
 export default App;
